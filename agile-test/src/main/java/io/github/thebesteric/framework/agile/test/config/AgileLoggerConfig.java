f0658cf9dba47e1f32c6030eb601cb073ac196c1
eric <whatisjava@hotmail.com>
2024-04-26 10:47:23 +0800

    test: test case

--- agile-test/src/main/java/io/github/thebesteric/framework/agile/test/config/AgileLoggerConfig.java
+++ agile-test/src/main/java/io/github/thebesteric/framework/agile/test/config/AgileLoggerConfig.java
@@ -2,11 +2,17 @@ package io.github.thebesteric.framework.agile.test.config;
 
 import io.github.thebesteric.framework.agile.plugins.logger.config.AgileLoggerProperties;
 import io.github.thebesteric.framework.agile.plugins.logger.domain.InvokeLog;
+import io.github.thebesteric.framework.agile.plugins.logger.domain.RequestLog;
+import io.github.thebesteric.framework.agile.plugins.logger.processor.ignore.RequestIgnoreProcessor;
+import io.github.thebesteric.framework.agile.plugins.logger.processor.ignore.impl.HeaderIgnoreProcessor;
+import io.github.thebesteric.framework.agile.plugins.logger.processor.ignore.impl.ParameterIgnoreProcessor;
 import io.github.thebesteric.framework.agile.plugins.logger.processor.recorder.Recorder;
 import io.github.thebesteric.framework.agile.plugins.logger.processor.recorder.impl.CustomRecorder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 /**
  * AgileLoggerConfig
@@ -25,6 +31,36 @@ public class AgileLoggerConfig {
             @Override
             protected void doProcess(InvokeLog invokeLog) {
                 System.out.println("This is my custom log: " + invokeLog.getLogId());
+            }
+        };
+    }
+
+    @Bean
+    public RequestIgnoreProcessor headerIgnoreProcessor() {
+        return new HeaderIgnoreProcessor() {
+            @Override
+            protected String[] doIgnore(RequestLog requestLog) {
+                return new String[]{"apple", "banana", "postman-token"};
+            }
+
+            @Override
+            protected Map<String, String> doRewrite(RequestLog requestLog) {
+                return Map.of("test", "test**");
+            }
+        };
+    }
+
+    @Bean
+    public RequestIgnoreProcessor parameterIgnoreProcessor() {
+        return new ParameterIgnoreProcessor() {
+            @Override
+            protected String[] doIgnore(RequestLog requestLog) {
+                return new String[]{"apple", "banana"};
+            }
+
+            @Override
+            protected Map<String, String> doRewrite(RequestLog requestLog) {
+                return Map.of("name", "name**");
             }
         };
     }
