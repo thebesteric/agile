9408cc3219bfe86d2de663da5035fcc3d0906f39
eric <whatisjava@hotmail.com>
2024-05-06 17:53:14 +0800

    refactor: 注解切点重构

---
+++ agile-core/src/main/java/io/github/thebesteric/framework/agile/core/pointcut/AbstractSpringComponentAnnotationPointcut.java
@@ -0,0 +1,65 @@
+package io.github.thebesteric.framework.agile.core.pointcut;
+
+import io.github.thebesteric.framework.agile.core.domain.PackageFinder;
+import io.github.thebesteric.framework.agile.core.matcher.clazz.ClassMatcher;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.support.JdkRegexpMethodPointcut;
+import org.springframework.lang.NonNull;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.List;
+
+/**
+ * 抽象的 Spring 组件的切点
+ *
+ * @author wangweijun
+ * @version v1.0
+ * @since 2024-05-06 17:39:22
+ */
+public abstract class AbstractSpringComponentAnnotationPointcut extends JdkRegexpMethodPointcut {
+
+    protected final List<ClassMatcher> classMatchers;
+
+    protected AbstractSpringComponentAnnotationPointcut(List<ClassMatcher> classMatchers) {
+        this.classMatchers = classMatchers;
+
+        // 创建正则表达式切点，匹配方法
+        String[] packageNames = PackageFinder.getPackageNames().toArray(new String[0]);
+        for (int i = 0; i < packageNames.length; i++) {
+            packageNames[i] = packageNames[i] + ".*";
+        }
+        this.setPatterns(packageNames);
+    }
+
+    @NonNull
+    @Override
+    public ClassFilter getClassFilter() {
+        return clazz -> {
+            for (Annotation annotation : clazz.getAnnotations()) {
+                for (ClassMatcher classMatcher : this.classMatchers) {
+                    if (classMatcher.matcher(annotation.annotationType())) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        };
+    }
+
+    @Override
+    public boolean matches(@NonNull Method method, @NonNull Class<?> targetClass) {
+        return method.isAnnotationPresent(matchedAnnotation());
+    }
+
+    /**
+     * 返回匹配的注解
+     *
+     * @return Class<Annotation> 匹配的注解
+     *
+     * @author wangweijun
+     * @since 2024/5/6 17:44
+     */
+    abstract protected Class<? extends Annotation> matchedAnnotation();
+
+}
--- agile-spring-boot-plugins/idempotent-plugin-parent/idempotent-plugin/src/main/java/io/github/thebesteric/framework/agile/plugins/idempotent/advisor/AgileIdempotentPointcut.java
+++ agile-spring-boot-plugins/idempotent-plugin-parent/idempotent-plugin/src/main/java/io/github/thebesteric/framework/agile/plugins/idempotent/advisor/AgileIdempotentPointcut.java
@@ -1,15 +1,10 @@
 package io.github.thebesteric.framework.agile.plugins.idempotent.advisor;
 
-import io.github.thebesteric.framework.agile.core.domain.PackageFinder;
-import io.github.thebesteric.framework.agile.core.matcher.clazz.ClassMatcher;
+import io.github.thebesteric.framework.agile.core.pointcut.AbstractSpringComponentAnnotationPointcut;
 import io.github.thebesteric.framework.agile.plugins.idempotent.annotation.Idempotent;
 import io.github.thebesteric.framework.agile.plugins.idempotent.config.AgileIdempotentContext;
-import org.springframework.aop.ClassFilter;
-import org.springframework.aop.support.JdkRegexpMethodPointcut;
-import org.springframework.lang.NonNull;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
 
 /**
  * AgileIdempotentPointcut
@@ -18,39 +13,15 @@ import java.lang.reflect.Method;
  * @version v1.0
  * @since 2024-04-30 15:29:43
  */
-public class AgileIdempotentPointcut extends JdkRegexpMethodPointcut {
-
-    private final AgileIdempotentContext context;
+public class AgileIdempotentPointcut extends AbstractSpringComponentAnnotationPointcut {
 
     public AgileIdempotentPointcut(AgileIdempotentContext context) {
-        this.context = context;
-
-        // 创建正则表达式切点，匹配方法
-        String[] packageNames = PackageFinder.getPackageNames().toArray(new String[0]);
-        for (int i = 0; i < packageNames.length; i++) {
-            packageNames[i] = packageNames[i] + ".*";
-        }
-        this.setPatterns(packageNames);
-    }
-
-    @NonNull
-    @Override
-    public ClassFilter getClassFilter() {
-        return clazz -> {
-            for (Annotation annotation : clazz.getAnnotations()) {
-                for (ClassMatcher classMatcher : context.getClassMatchers()) {
-                    if (classMatcher.matcher(annotation.annotationType())) {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        };
+        super(context.getClassMatchers());
     }
 
     @Override
-    public boolean matches(@NonNull Method method, @NonNull Class<?> targetClass) {
-        return method.isAnnotationPresent(Idempotent.class);
+    protected Class<? extends Annotation> matchedAnnotation() {
+        return Idempotent.class;
     }
 
 }
