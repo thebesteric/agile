14fefc57f9b10731ccae56d93f8e5397f8913313
eric <whatisjava@hotmail.com>
2024-08-23 10:23:52 +0800

    pref: 优化限流和幂等插件，无需手动创建插件

---
+++ agile-spring-boot-plugins/idempotent-plugin-parent/idempotent-plugin-redis/src/main/java/io/github/thebesteric/framework/agile/plugins/idempotent/redis/config/AgileIdempotentRedisAutoConfiguration.java
@@ -0,0 +1,32 @@
+package io.github.thebesteric.framework.agile.plugins.idempotent.redis.config;
+
+import io.github.thebesteric.framework.agile.core.AgileConstants;
+import io.github.thebesteric.framework.agile.plugins.idempotent.config.AgileIdempotentProperties;
+import io.github.thebesteric.framework.agile.plugins.idempotent.processor.IdempotentProcessor;
+import io.github.thebesteric.framework.agile.plugins.idempotent.redis.processor.impl.RedisIdempotentProcessor;
+import org.redisson.api.RedissonClient;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * AgileIdempotentRedisAutoConfiguration
+ *
+ * @author wangweijun
+ * @version v1.0
+ * @since 2024-08-23 10:03:16
+ */
+@Configuration
+@EnableConfigurationProperties(AgileIdempotentProperties.class)
+@ConditionalOnProperty(prefix = AgileConstants.PROPERTIES_PREFIX + ".idempotent", name = "enable", havingValue = "true", matchIfMissing = true)
+public class AgileIdempotentRedisAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(IdempotentProcessor.class)
+    public IdempotentProcessor redisIdempotentProcessor(RedissonClient redissonClient) {
+        return new RedisIdempotentProcessor(redissonClient);
+    }
+
+}
---
+++ agile-spring-boot-plugins/limiter-plugin-parent/limiter-plugin-redis/src/main/java/io/github/thebesteric/framework/agile/plugins/limiter/redis/config/AgileRateLimiterRedisAutoConfiguration.java
@@ -0,0 +1,32 @@
+package io.github.thebesteric.framework.agile.plugins.limiter.redis.config;
+
+import io.github.thebesteric.framework.agile.core.AgileConstants;
+import io.github.thebesteric.framework.agile.plugins.limiter.config.AgileRateLimiterProperties;
+import io.github.thebesteric.framework.agile.plugins.limiter.processor.RateLimiterProcessor;
+import io.github.thebesteric.framework.agile.plugins.limiter.redis.processor.impl.RedisRateLimiterProcessor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * AgileRateLimiterRedisAutoConfiguration
+ *
+ * @author wangweijun
+ * @version v1.0
+ * @since 2024-05-06 17:21:50
+ */
+@Configuration
+@EnableConfigurationProperties(AgileRateLimiterProperties.class)
+@ConditionalOnProperty(prefix = AgileConstants.PROPERTIES_PREFIX + ".limiter", name = "enable", havingValue = "true", matchIfMissing = true)
+public class AgileRateLimiterRedisAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(RateLimiterProcessor.class)
+    public RateLimiterProcessor redisRateLimiterProcessor(RedisTemplate<String, Object> redisTemplate) {
+        return new RedisRateLimiterProcessor(redisTemplate);
+    }
+
+}
