99b4476b1a954dec1995ac5d43df1738a02ce487
eric <whatisjava@hotmail.com>
2024-11-06 14:54:09 +0800

    test: test case

--- agile-test/src/test/java/io/github/thebesteric/framework/agile/test/workflow/WorkflowServiceTest.java
+++ agile-test/src/test/java/io/github/thebesteric/framework/agile/test/workflow/WorkflowServiceTest.java
@@ -1,11 +1,17 @@
 package io.github.thebesteric.framework.agile.test.workflow;
 
+import io.github.thebesteric.framework.agile.commons.util.MapWrapper;
 import io.github.thebesteric.framework.agile.plugins.database.core.domain.query.OrderByOperator;
 import io.github.thebesteric.framework.agile.plugins.database.core.domain.query.builder.Query;
 import io.github.thebesteric.framework.agile.plugins.database.core.domain.query.builder.QueryBuilderWrapper;
 import io.github.thebesteric.framework.agile.plugins.workflow.WorkflowEngine;
+import io.github.thebesteric.framework.agile.plugins.workflow.domain.BusinessInfo;
+import io.github.thebesteric.framework.agile.plugins.workflow.domain.Requester;
 import io.github.thebesteric.framework.agile.plugins.workflow.entity.WorkflowDefinition;
 import io.github.thebesteric.framework.agile.plugins.workflow.entity.WorkflowInstance;
+import io.github.thebesteric.framework.agile.plugins.workflow.helper.WorkflowHelper;
+import io.github.thebesteric.framework.agile.plugins.workflow.helper.service.DeploymentServiceHelper;
+import io.github.thebesteric.framework.agile.plugins.workflow.helper.service.RuntimeServiceHelper;
 import io.github.thebesteric.framework.agile.plugins.workflow.service.WorkflowService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,5 +52,20 @@ class WorkflowServiceTest {
                 .build();
         List<WorkflowInstance> workflowInstances = workflowService.findWorkflowInstances(query).getRecords();
         workflowInstances.forEach(System.out::println);
+    }
+
+    @Test
+    void submit() {
+        WorkflowHelper workflowHelper = new WorkflowHelper(workflowEngine);
+
+        DeploymentServiceHelper deploymentServiceHelper = workflowHelper.getDeploymentServiceHelper();
+        WorkflowDefinition workflowDefinition = deploymentServiceHelper.getById("8888", 1);
+
+        RuntimeServiceHelper runtimeServiceHelper = workflowHelper.getRuntimeServiceHelper();
+        Requester requester = Requester.of("1000", "eric");
+        BusinessInfo businessInfo = BusinessInfo.of(MapWrapper.create().put("businessId", 1000L).put("businessType", 1).build());
+        WorkflowInstance workflowInstance = runtimeServiceHelper.start(workflowDefinition, requester, businessInfo, "测试审批");
+
+        System.out.println(workflowInstance);
     }
 }
