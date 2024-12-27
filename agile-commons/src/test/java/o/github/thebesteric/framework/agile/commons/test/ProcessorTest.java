cea89691aa86d8f4ab7466d851013f1fcafefb76
eric <whatisjava@hotmail.com>
2024-12-27 15:19:40 +0800

    test: test case

---
+++ agile-commons/src/test/java/o/github/thebesteric/framework/agile/commons/test/ProcessorTest.java
@@ -0,0 +1,29 @@
+package o.github.thebesteric.framework.agile.commons.test;
+
+import io.github.thebesteric.framework.agile.commons.util.Processor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.DataValidationException;
+
+/**
+ * o.github.thebesteric.framework.agile.commons.test.ProcessorTest
+ *
+ * @author wangweijun
+ * @version v1.0
+ * @since 2024-12-27 15:12:20
+ */
+class ProcessorTest {
+
+    @Test
+    void test() {
+        Assertions.assertThrows(RuntimeException.class, () -> Processor.prepare(String.class)
+                .start(() -> "hello world")
+                .validate(s -> {
+                    throw new DataValidationException("s.length() > 5");
+                })
+                .complete(s -> {
+                    System.out.println(s);
+                }));
+    }
+
+}
