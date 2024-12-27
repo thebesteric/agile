591aeab67385e99f1fbee8869eb5396959e4366d
eric <whatisjava@hotmail.com>
2024-12-27 09:34:28 +0800

    perf: Processor 优化执行逻辑，减少强转

--- agile-commons/src/main/java/io/github/thebesteric/framework/agile/commons/util/Processor.java
+++ agile-commons/src/main/java/io/github/thebesteric/framework/agile/commons/util/Processor.java
@@ -16,47 +16,34 @@ import java.util.function.*;
 public class Processor<T> {
 
     private T object;
+    private final Class<T> objectType;
+    private final Class<? extends RuntimeException> defaultExceptionClass;
+    private final DataValidator.ExceptionThrowStrategy exceptionThrowStrategy;
     private final DataValidator dataValidator;
     private final List<Predicate<List<RuntimeException>>> exceptionListeners = new ArrayList<>();
 
 
-    private <E extends RuntimeException> Processor(Class<E> defaultExceptionClass, DataValidator.ExceptionThrowStrategy exceptionThrowStrategy) {
-        dataValidator = DataValidator.create(defaultExceptionClass, exceptionThrowStrategy);
+    private <E extends RuntimeException> Processor(Class<T> initialObjectType, Class<E> defaultExceptionClass, DataValidator.ExceptionThrowStrategy exceptionThrowStrategy) {
+        this.objectType = initialObjectType;
+        this.defaultExceptionClass = defaultExceptionClass;
+        this.exceptionThrowStrategy = exceptionThrowStrategy;
+        this.dataValidator = DataValidator.create(defaultExceptionClass, exceptionThrowStrategy);
     }
 
-    public static <T> Processor<T> prepare() {
-        return io.github.thebesteric.framework.agile.commons.util.Processor.prepare(DataValidator.ExceptionThrowStrategy.IMMEDIATELY);
+    public static <T> Processor<T> prepare(Class<T> initialObjectType) {
+        return Processor.prepare(initialObjectType, DataValidator.ExceptionThrowStrategy.IMMEDIATELY);
     }
 
-    public static <T> Processor<T> prepare(DataValidator.ExceptionThrowStrategy exceptionThrowStrategy) {
-        return Processor.prepare(RuntimeException.class, exceptionThrowStrategy);
+    public static <T> Processor<T> prepare(Class<T> initialObjectType, DataValidator.ExceptionThrowStrategy exceptionThrowStrategy) {
+        return Processor.prepare(initialObjectType, RuntimeException.class, exceptionThrowStrategy);
     }
 
-    public static <T, E extends RuntimeException> Processor<T> prepare(Class<E> defaultExceptionClass, DataValidator.ExceptionThrowStrategy exceptionThrowStrategy) {
-        return new Processor<>(defaultExceptionClass, exceptionThrowStrategy);
-    }
-
-    public Processor<T> registerExceptionListener(Predicate<List<RuntimeException>> exceptionListener) {
-        exceptionListeners.add(exceptionListener);
-        return this;
-    }
-
-    public <E extends RuntimeException> Processor<T> setDefaultExceptionClass(Class<E> defaultExceptionClass) {
-        dataValidator.setDefaultExceptionClass(defaultExceptionClass);
-        return this;
+    public static <T, E extends RuntimeException> Processor<T> prepare(Class<T> initialObjectType, Class<E> defaultExceptionClass, DataValidator.ExceptionThrowStrategy exceptionThrowStrategy) {
+        return new Processor<>(initialObjectType, defaultExceptionClass, exceptionThrowStrategy);
     }
 
     public Processor<T> start(Supplier<T> supplier) {
         this.object = supplier.get();
-        return this;
-    }
-
-    public Processor<T> next(Supplier<T> supplier) {
-        return start(supplier);
-    }
-
-    public Processor<T> next(UnaryOperator<T> unary) {
-        this.object = unary.apply(this.object);
         return this;
     }
 
@@ -76,6 +63,19 @@ public class Processor<T> {
         return this;
     }
 
+    public Processor<T> next(Supplier<T> supplier) {
+        return start(supplier);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <R> Processor<R> next(Function<T, R> function) {
+        R r = function.apply(this.object);
+        Class<R> changedObjectType = (Class<R>) r.getClass();
+        Processor<R> processor = new Processor<>(changedObjectType, this.defaultExceptionClass, this.exceptionThrowStrategy);
+        processor.object = r;
+        return processor;
+    }
+
     public void complete(Consumer<T> consumer) {
         List<RuntimeException> exceptions = dataValidator.getExceptions();
         if (CollectionUtils.isEmpty(exceptions)) {
@@ -85,16 +85,26 @@ public class Processor<T> {
         throwExceptionsIfNecessary();
     }
 
-    public T complete(UnaryOperator<T> unary) {
+    public <R> R complete(Function<T, R> function) {
         List<RuntimeException> exceptions = dataValidator.getExceptions();
-        T result = null;
+        R result = null;
         if (CollectionUtils.isEmpty(exceptions)) {
-            result = Try.of(() -> unary.apply(this.object))
+            result = Try.of(() -> function.apply(this.object))
                     .onFailure(e -> exceptions.add(new RuntimeException(e)))
                     .get();
         }
         throwExceptionsIfNecessary();
         return result;
+    }
+
+    public Processor<T> registerExceptionListener(Predicate<List<RuntimeException>> exceptionListener) {
+        exceptionListeners.add(exceptionListener);
+        return this;
+    }
+
+    public <E extends RuntimeException> Processor<T> setDefaultExceptionClass(Class<E> defaultExceptionClass) {
+        dataValidator.setDefaultExceptionClass(defaultExceptionClass);
+        return this;
     }
 
     private void throwExceptionsIfNecessary() {
