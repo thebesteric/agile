820e2fa2d7847adaf5293dd5f8ce5a7bf258f875
eric <whatisjava@hotmail.com>
2025-01-24 17:38:46 +0800

    test: test case

---
+++ agile-test/src/main/java/io/github/thebesteric/framework/agile/test/controller/MockerController.java
@@ -0,0 +1,72 @@
+package io.github.thebesteric.framework.agile.test.controller;
+
+import io.github.thebesteric.framework.agile.core.domain.R;
+import io.github.thebesteric.framework.agile.plugins.logger.annotation.AgileLogger;
+import io.github.thebesteric.framework.agile.plugins.mocker.annotation.Mock;
+import io.github.thebesteric.framework.agile.plugins.mocker.mocker.MockType;
+import io.github.thebesteric.framework.agile.test.mock.MyMocker;
+import lombok.Data;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * MockerController
+ *
+ * @author wangweijun
+ * @version v1.0
+ * @since 2025-01-23 18:10:10
+ */
+@RestController
+@RequestMapping("/mocker")
+@AgileLogger(tag = "hello")
+public class MockerController {
+
+    @Mock(condition = "(#parent.id == 1 || #parent.sub.name == lisi) && #name == zs", type = MockType.CLASS, targetClass = MyMocker.class)
+    @AgileLogger
+    @PostMapping("/method1")
+    public R<String> method1(@RequestParam(required = false) String name, @RequestBody Parent parent) {
+        return R.success(name);
+    }
+
+    @Mock(condition = "(#parent.id == 1 || #parent.sub.name == lisi) && #name == zs", type = MockType.FILE, path = "classpath:mock/mock-method2.json")
+    @AgileLogger
+    @PostMapping("/method2")
+    public R<String> method2(@RequestParam(required = false) String name, @RequestBody Parent parent) {
+        return R.success(name);
+    }
+
+    @Mock(condition = "(#parent.id == 1 || #parent.sub.name == lisi) && #name == zs", type = MockType.FILE, path = "file:/Users/wangweijun/Downloads/mock-file.json")
+    @AgileLogger
+    @PostMapping("/method3")
+    public R<String> method3(@RequestParam(required = false) String name, @RequestBody Parent parent) {
+        return R.success(name);
+    }
+
+    @Mock(condition = "(#parent.id == 1 || #parent.sub.name == lisi) && #name == zs", type = MockType.URL, path = "http://127.0.0.1:8080/mocker/test")
+    @AgileLogger
+    @PostMapping("/method4")
+    public R<String> method4(@RequestParam(required = false) String name, @RequestBody Parent parent) {
+        return R.success(name);
+    }
+
+    @AgileLogger
+    @GetMapping("/test")
+    public R<String> test() {
+        return R.success("test");
+    }
+
+    @Data
+    public static class Parent {
+
+        private Integer id;
+        private String name;
+        private Sub sub;
+
+        @Data
+        public static class Sub {
+            private Integer id;
+            private String name;
+        }
+    }
+
+
+}
